558200258e26664e91b683c703a93e6b4b1296d2
rahul dutt sharma <untoldbytes@gmail.com>
2021-12-27 14:16:19 +0530

    merge

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,19 +3,19 @@ import { Link } from "react-router-dom";
 
 import Edit from './Edit';
 
-export default function Card({logo, artwork, text, changeText, editUrl}) {
+export default function Card({logo, artwork, text, changeText, editUrl, showLogo}) {
     return (
             <div className="card">
                 <Link to={editUrl}>
-                    <img className="logo" src={logo} alt="" />
+                  {showLogo? <img className="logo" src={logo} alt="" />: null}
                     <div className="text">{text}</div>
                     <img className="artwork" src={artwork} alt="" />
                     {
                     changeText && <span className='edit-text'>Customise &nbsp;<Edit/></span>
-                    
+
                     }
                 </Link>
             </div>
-        
+
     )
 }
