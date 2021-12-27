398d4895e42034c6966508690826750dc0ca3492
kumar247001 <kumar247001@gmail.com>
2021-12-27 20:18:37 +0530

    Added socks lottie animation

--- src/components/Card2.jsx
+++ src/components/Card2.jsx
@@ -1,16 +1,22 @@
 import React from 'react'
 import { Link } from "react-router-dom";
 
+import { Player, Controls } from '@lottiefiles/react-lottie-player';
+import SocksAnimation from '../lottie/socks-anim.json';
+
 import Edit from './Edit';
 
-export default function Card2({logo, artwork, text, changeText, editUrl, showLogo}) {
+export default function Card2({logo, text, changeText, editUrl, showLogo}) {
     return (
             <div className="card2">
                 <Link to={editUrl}>
                     <div className="inner">
                     {showLogo? <img className="logo" src={logo} alt="" />: null}
                         <div className="text">{text}</div>
-                        <img className="artwork" src={artwork} alt="" />
+                        <Player className="artwork" autoplay loop src={SocksAnimation}
+                        style={{ height: '80px', width: '80px' }}>
+                        <Controls visible={false} />
+                        </Player>
                     </div>
                     {
                         changeText && <span className='edit-text'>Customise &nbsp;<Edit/></span>
